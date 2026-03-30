4d59ee1f14f8de0e8bd4208414f8f76b7a1bf035
DANFOLD <danfordmos@gmail.com>
2026-03-30 15:27:29 +0300

    Fixing stuff

--- frontend/config.js
+++
@@ -1,8 +0,0 @@
-window.ELIMUCOIN_CONFIG = {
-  senseiBase: "http://localhost:8000",
-  radarBase: "http://localhost:8001",
-  biometricBase: "http://localhost:8002",
-  fallbackBase: "http://localhost:9000",
-  useDemoServer: true,
-  requestTimeoutMs: 5000
-};
